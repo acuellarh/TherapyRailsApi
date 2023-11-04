6620c867aa62803f56615174e5d5567b00d1508f
Alejandro <acuellarh@gmail.com>
2023-11-04 10:01:25 -0500

    Add Exception handling in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_destroyed
+    render json: { errors: e.record.errors }, status: :unproccessable_entity
+  end
 end
+
